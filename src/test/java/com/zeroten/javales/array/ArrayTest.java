d2f0bf6d97840e5f0a861442ae7269b82f960523
lxq <2535807845@qq.com>
2020-01-08 18:20:13 +0800

    数组遍历

--- src/test/java/com/zeroten/javales/array/ArrayTest.java
+++ src/test/java/com/zeroten/javales/array/ArrayTest.java
@@ -41,4 +41,29 @@ public class ArrayTest {
         Integer[] arr8 = {2, 5, 6, null, 0, 5};
         System.out.println(Arrays.toString(arr8));
     }
+
+    @Test
+    public void testBianLi(){
+        Integer[] arr = {1, 2, 3, 4, 5};
+        //for
+        for (int i = 0; i < arr.length; i++){
+            //System.out.println("下标：" + i + "，存放的值是：" + arr[i]);
+            System.out.println(String.format("下标：%d, 存放的值是：%d", i, arr[i]));
+        }
+        //foreach
+        int index = 0;
+        for (int val : arr){
+            System.out.println(String.format("第 %d 次取到 %d", index, val));
+            index++;
+        }
+        //lambda表达式
+        Arrays.asList(arr).forEach(val -> {
+            System.out.println(String.format("值是 %d", val));
+            if(val % 2 == 0){
+                System.out.println("偶数");
+            } else {
+                System.out.println("奇数");
+            }
+        });
+    }
 }
